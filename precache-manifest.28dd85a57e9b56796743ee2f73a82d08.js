65bde39912d79bebb303f477390bb389ea337430
Stefán Arnar Einarsson <stebbi30@gmail.com>
2020-01-05 23:41:54 +0000

    react

---
+++ precache-manifest.28dd85a57e9b56796743ee2f73a82d08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1e8640993d47fca913657815083f8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dae5c14bbb80041c399f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a52608e16b4a9b1b2ecb",
+    "url": "/static/js/2.a88dee15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a88dee15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dae5c14bbb80041c399f",
+    "url": "/static/js/main.6df5a56f.chunk.js"
+  },
+  {
+    "revision": "087fbabd3916d2e6ece3",
+    "url": "/static/js/runtime-main.77f083e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
